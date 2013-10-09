4056fd2650c59d8c8183607797ddbc4342e2281f
Carles <carles@jeannine-VGN-S5M-S.(none)>
2013-10-09 19:28:29 +0200

    commit

---
+++ examples/isl29023-demo.ino
@@ -0,0 +1,23 @@
+#include <isl29023.h>
+#include <Wire.h>
+
+
+
+Isl29023 isl = ISL29023();
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Wire.begin();
+  cl.init(); 
+}
+
+void loop() {
+  float light;
+  // put your main code here, to run repeatedly: 
+  for(;;){
+    isl.read(&light);
+    Serial.println(light);
+  }
+}
+
